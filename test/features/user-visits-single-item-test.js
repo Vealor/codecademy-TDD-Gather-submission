21c49fb7a3b922e50a01284c8ef560416aba7c31
Vealor <jakob.m.roberts@gmail.com>
2018-01-12 10:53:03 +0900

    Fix indentation

--- test/features/user-visits-single-item-test.js
+++ test/features/user-visits-single-item-test.js
@@ -2,19 +2,19 @@ const {assert} = require('chai');
 const {buildItemObject} = require('../test-utils');
 
 describe('User visits a single item', () => {
-    describe('user views the single item', () => {
-      it('and is rendered', () => {
-        const itemToCreate = buildItemObject();
-        browser.url('/items/create');
-        browser.setValue('#title-input', itemToCreate.title);
-        browser.setValue('#description-input', itemToCreate.description);
-        browser.setValue('#imageUrl-input', itemToCreate.imageUrl);
-        browser.click('#submit-button');
+  describe('user views the single item', () => {
+    it('and is rendered', () => {
+      const itemToCreate = buildItemObject();
+      browser.url('/items/create');
+      browser.setValue('#title-input', itemToCreate.title);
+      browser.setValue('#description-input', itemToCreate.description);
+      browser.setValue('#imageUrl-input', itemToCreate.imageUrl);
+      browser.click('#submit-button');
 
-        browser.click('.item-card a'); 
+      browser.click('.item-card a'); 
 
-        assert.include(browser.getText('body'), itemToCreate.description);
-      });
+      assert.include(browser.getText('body'), itemToCreate.description);
     });
+  });
 });
 
